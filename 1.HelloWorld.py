9747dc9febf3dd99fc65f5770a267c5ccd88f3b3
Amit Kumar Maiti <53132692+amit-maiti@users.noreply.github.com>
2024-06-02 20:54:12 +0530

    Delete 1.HelloWorld.py

--- 1.HelloWorld.py
+++
@@ -1,6 +0,0 @@
-# Write your  first piece of code
-print("Hello, Wworld!")
-print(""" Hello Tim, Hope you are doing well.
-    Let's come and do some cool stuff with python.
-    Doing Data Science project, Building web application, 
-    developing games using python would be adventurous.""")
